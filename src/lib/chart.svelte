e37d7f35b76e7cc8789426e9ab8de8442c8e0827
R.E <rechezuria@gmail.com>
2023-02-17 17:01:05 -0400

    feat: :sparkles: Add the dataStore

--- src/lib/chart.svelte
+++ src/lib/chart.svelte
@@ -1,6 +1,9 @@
 <script>
+	import { darien } from '../stores/dataStore'
 	import { boxSize } from '../stores/size-store'
 	import Svg from './svg.svelte'
+
+	console.log('darien :>> ', $darien)
 </script>
 
 <Svg svgWidth={$boxSize.width} svgHeight={$boxSize.height}>
