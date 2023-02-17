a83f022dadd69e012d7b1a939fc291cd23c9473b
R.E <rechezuria@gmail.com>
2023-02-17 11:51:48 -0400

    feat: :sparkles: Create the svg component

---
+++ src/lib/svg.svelte
@@ -0,0 +1,13 @@
+<script>
+	export let svgWidth, svgHeight
+</script>
+
+<svg width={svgWidth} height={svgHeight}>
+	<slot />
+</svg>
+
+<style>
+	svg {
+		outline: 1px solid cornflowerblue;
+	}
+</style>
