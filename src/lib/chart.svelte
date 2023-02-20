e270beeb7431fb4d20b4a65f242156ec0261c615
R.E <rechezuria@gmail.com>
2023-02-20 12:05:28 -0400

    refactor: Add x-Axis component

--- src/lib/chart.svelte
+++ src/lib/chart.svelte
@@ -2,12 +2,13 @@
 	import { darien } from '../stores/dataStore'
 	import { boxSize } from '../stores/size-store'
 	import Svg from './svg.svelte'
+	import XAxis from './x-axis.svelte'
 
-	console.log('darien :>> ', $darien)
+	const margins = { top: 35, right: 35, bottom: 35, left: 35 }
 </script>
 
 <Svg svgWidth={$boxSize.width} svgHeight={$boxSize.height}>
-	<!-- test -->
+	<XAxis data={$darien} parentSize={$boxSize} {margins} />
 </Svg>
 
 <style>
---
+++ src/lib/x-axis.svelte
@@ -0,0 +1,22 @@
+<script>
+	import { scaleBand } from 'd3'
+
+	export let data, margins, parentSize
+	const { width, height } = parentSize
+	const { top, right, bottom, left } = margins
+	const years = Array.from(new Set(data.map((d) => d.year)))
+	const x = scaleBand()
+		.domain(years)
+		.round(true)
+		.paddingOuter(0.15)
+		.paddingInner(0.65)
+		.range([left, width - right])
+</script>
+
+{#each years as year}
+	<text x={x(year)} y={height - bottom}>{year}</text>
+{/each}
+
+<style>
+	/* your styles go here */
+</style>
