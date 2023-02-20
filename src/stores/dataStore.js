8c4f957e0055d2bb2e9055526ef70c053f2ca24c
R.E <rechezuria@gmail.com>
2023-02-20 11:02:38 -0400

    refactor: :monocle_face: Transform data

--- src/stores/dataStore.js
+++ src/stores/dataStore.js
@@ -1,4 +1,10 @@
-import { csv, group } from 'd3'
+import {
+	csv,
+	group,
+	stack,
+	stackOrderAscending,
+	stackOrderDescending,
+} from 'd3'
 import { writable } from 'svelte/store'
 
 export const darien = writable()
@@ -17,11 +23,31 @@ let resp = await csv(
 resp = Array.from(
 	group(resp, (d) => d.year),
 	([key, value]) => {
+		let obj = {}
+		let values = value
+			.sort((a, b) => b.total - a.total)
+			.forEach((d) => {
+				Object.assign(obj, { [d.country]: d.total })
+			})
 		return {
-			key,
-			values: value.sort((a, b) => b.total - a.total),
+			year: key,
+			...obj,
 		}
 	}
 )
-console.log('resp :>> ', resp)
-darien.set(resp)
+const dataKeys = Object.keys(resp.at(0)).slice(1)
+const stackGen = stack().keys(dataKeys).order(stackOrderAscending)
+resp = stackGen(resp).map((d) => {
+	const arr = d.map((a) => {
+		return {
+			from: a.at(0),
+			to: a.at(1),
+			totalValue: a.data[d.key],
+			year: a.data.year,
+			country: d.key,
+		}
+	})
+
+	return arr
+})
+darien.set(resp.flat())
