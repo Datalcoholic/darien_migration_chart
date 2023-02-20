51c8be77c373c2c7548f2d282cce392ea52b4800
R.E <rechezuria@gmail.com>
2023-02-20 16:03:13 -0400

    feat: :sparkles: Add y.axis component

--- src/lib/chart.svelte
+++ src/lib/chart.svelte
@@ -3,12 +3,14 @@
 	import { boxSize } from '../stores/size-store'
 	import Svg from './svg.svelte'
 	import XAxis from './x-axis.svelte'
+	import YAxis from './yAxis/y-axis.svelte'
 
-	const margins = { top: 35, right: 35, bottom: 35, left: 35 }
+	const margins = { top: 35, right: 50, bottom: 35, left: 25 }
 </script>
 
 <Svg svgWidth={$boxSize.width} svgHeight={$boxSize.height}>
-	<XAxis data={$darien} parentSize={$boxSize} {margins} />
+	<XAxis data={$darien} {margins} />
+	<YAxis data={$darien} {margins} />
 </Svg>
 
 <style>
