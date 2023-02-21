adf252d7a6bf62243ddfa5ddb8e3e68564410e6f
R.E <rechezuria@gmail.com>
2023-02-21 15:22:16 -0400

    refactor: :recycle: send x scale to the store

--- src/lib/x-axis.svelte
+++ src/lib/x-axis.svelte
@@ -1,21 +1,28 @@
 <script>
 	import { scaleBand } from 'd3'
+	import { x } from '../stores/scalesStores'
 	import { boxSize } from '../stores/size-store'
 
 	export let data, margins
 	const { top, right, bottom, left } = margins
 	const years = Array.from(new Set(data.map((d) => d.year)))
-	$: x = scaleBand()
+	$: xScale = scaleBand()
 		.domain(years)
 		.round(true)
 		.paddingOuter(0.15)
 		.paddingInner(0.65)
 		.range([left, $boxSize.width - right])
+
+	x.set(xScale)
+	$: console.log('x :>> ', x)
 </script>
 
-{#each years as year}
-	<text x={x(year)} y={$boxSize.height - bottom}>{year}</text>
-{/each}
+{#if $x}
+	<!-- content here -->
+	{#each years as year}
+		<text x={$x(year)} y={$boxSize.height - bottom}>{year}</text>
+	{/each}
+{/if}
 
 <style>
 	/* your styles go here */
