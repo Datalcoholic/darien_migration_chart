b6ea21846f27f124ab32114073ca7e3123f1523c
R.E <rechezuria@gmail.com>
2023-02-22 12:58:57 -0400

    feat: :sparkles: Add area component

---
+++ src/lib/area.svelte
@@ -0,0 +1,67 @@
+<script>
+	import {
+		area,
+		curveMonotoneX,
+		extent,
+		group,
+		scaleLinear,
+		scaleOrdinal,
+	} from 'd3'
+	import { darien } from '../stores/dataStore'
+	import { x, y } from '../stores/scalesStores'
+
+	const countryColors = {
+		atomic_tangerine: 'hsla(19, 99%, 71%, 1)',
+		ash_gray: 'hsla(72, 15%, 74%, 1)',
+		air_superiority_blue: 'hsla(199, 46%, 56%, 1)',
+		carolina_blue: 'hsla(201, 51%, 69%, 1)',
+		columbia_blue: 'hsla(198, 56%, 84%, 1)',
+		tomato: 'hsla(8, 99%, 65%, 1)',
+		sunset: 'hsla(36, 98%, 77%, 1)',
+	}
+
+	const colorNames = Object.keys(countryColors)
+	const countryDomain = Array.from(new Set($darien.map((d) => d.country)))
+	const fillPalette = scaleOrdinal().domain(countryDomain).range(colorNames)
+	const valuesExt = extent($darien, (d) => d.totalValue)
+
+	// Para que el grafico apilado no sobrepase al eje se utiliza una scalal linear donde el dominio es desde 0 hasta el valos maximos de los valores despues de apilar y el rango es desde 0 hasta la sumatoria de los valores
+	$: yScale = scaleLinear().domain([0, 248284]).range([0, 404516])
+
+	const seriesWidth = 95
+	// Se generan 2 puntos extras unos antes del punto y otro despues del puntos del eje x
+	$: transformedData = $darien.map((d) => {
+		const before = { ...d, x: $x(d.year) - seriesWidth }
+		const after = { ...d, x: $x(d.year) + seriesWidth }
+		return [before, after]
+	})
+	$: areaGen = area()
+		.x((d) => d.x)
+		.y0((d) => $y(yScale(d.from)))
+		.y1((d) => $y(yScale(d.to)))
+		.curve(curveMonotoneX)
+
+	$: groupedDataNew = Array.from(
+		group(transformedData.flat(2), (d) => d.country),
+		([key, value]) => {
+			const d = areaGen(value)
+			return {
+				key,
+				d,
+				fill: fillPalette(key),
+			}
+		}
+	)
+</script>
+
+<g class="area-series">
+	{#each groupedDataNew as d}
+		<path d={d.d} fill={countryColors[d.fill]} />
+	{/each}
+</g>
+
+<style>
+	.area-series {
+		stroke: white;
+	}
+</style>
