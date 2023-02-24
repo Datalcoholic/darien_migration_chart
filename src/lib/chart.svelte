856e6bef5562e64458d5b4e26e55fd78b7c774e0
R.E <rechezuria@gmail.com>
2023-02-24 10:11:32 -0400

    feat: Add quadtree component

--- src/lib/chart.svelte
+++ src/lib/chart.svelte
@@ -2,17 +2,19 @@
 	import { darien } from '../stores/dataStore'
 	import { boxSize } from '../stores/size-store'
 	import Area from './area.svelte'
+	import Quadtree from './quadtree/quadtree.svelte'
 	import Svg from './svg.svelte'
 	import XAxis from './x-axis.svelte'
 	import YAxis from './yAxis/y-axis.svelte'
 
-	const margins = { top: 35, right: 20, bottom: 35, left: 15 }
+	const margins = { top: 35, right: 20, bottom: 35, left: 75 }
 </script>
 
 <Svg svgWidth={$boxSize.width} svgHeight={$boxSize.height}>
 	<Area {margins} />
 	<YAxis data={$darien} {margins} />
 	<XAxis data={$darien} {margins} />
+	<!-- <Quadtree {margins} /> -->
 </Svg>
 
 <style>
